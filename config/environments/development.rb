df3c6226e5b471f209cbdb810a5230023e6cf280
Alex314064 <135997704+Alex314064@users.noreply.github.com>
2023-12-06 16:07:42 +0100

    mailer update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
