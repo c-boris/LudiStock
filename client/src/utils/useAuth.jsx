3695f6edc569be489685ed872a385181292fa6ac
cboris <b.crochet@gmail.com>
2023-12-06 10:22:23 +0100

    test prod

--- client/src/utils/useAuth.jsx
+++ client/src/utils/useAuth.jsx
@@ -3,7 +3,7 @@ import { useAtom } from "jotai";
 import Cookies from "js-cookie";
 import { userAtom } from "./atom";
 
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/api';
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '';
 // const API_URL = "http://localhost:3001";
 // const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : '/api';
 
