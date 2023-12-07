86a5a536f698652372dbad94bf00372648e770b0
cboris <b.crochet@gmail.com>
2023-12-07 14:23:35 +0100

    logout redirection fix

--- client/src/utils/CookieManager.jsx
+++ client/src/utils/CookieManager.jsx
@@ -1,18 +1,18 @@
 // CookieManager.js
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
-const setAuthCookies = (token, userData) => {
-  Cookies.set("token", token);
-  Cookies.set("id", userData.id);
-  Cookies.set("email", userData.email);
-  Cookies.set("username", userData.username);
-};
+// const setAuthCookies = (token, userData) => {
+//   Cookies.set("token", token);
+//   Cookies.set("id", userData.id);
+//   Cookies.set("email", userData.email);
+//   Cookies.set("username", userData.username);
+// };
 
-const clearAuthCookies = () => {
-  Cookies.remove("token");
-  Cookies.remove("id");
-  Cookies.remove("email");
-  Cookies.remove("username");
-};
+// const clearAuthCookies = () => {
+//   Cookies.remove("token");
+//   Cookies.remove("id");
+//   Cookies.remove("email");
+//   Cookies.remove("username");
+// };
 
-export { setAuthCookies, clearAuthCookies };
+// export { setAuthCookies, clearAuthCookies };
