928750104a3fc786447f4e3378be3e901227cc64
cboris <b.crochet@gmail.com>
2023-12-11 12:27:08 +0100

    change from rails credentials to dotenv-rails

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -20,8 +20,17 @@ end
 
   def get_user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
-                             Rails.application.credentials.devise[:jwt_secret_key]).first
+    ENV['DEVISE_JWT_SECRET_KEY']).first
     user_id = jwt_payload['sub']
     User.find(user_id.to_s)
   end
 end
+
+#   def get_user_from_token
+#     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+#                              Rails.application.credentials.devise[:jwt_secret_key]).first
+#     user_id = jwt_payload['sub']
+#     User.find(user_id.to_s)
+#   end
+# end
+
