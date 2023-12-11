9fb73335421243610b5c3fb2b3121631737d7a0b
cboris <b.crochet@gmail.com>
2023-12-11 21:49:02 +0100

    edit profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,17 +5,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_profile
     authenticate_user!
-    
-    if current_user.update(user_params)
-      render json: {
-        message: 'Profile updated successfully.',
-        user: current_user
-      }, status: :ok
+
+    # Ensure the password is provided in the request
+    if params[:user][:password].blank?
+      render json: { error: 'Password is required to update profile' }, status: :unprocessable_entity
+      return
+    end
+
+    # Check if the provided password is valid
+    if current_user.valid_password?(params[:user][:password])
+      if current_user.update(user_params)
+        render json: {
+          message: 'Profile updated successfully.',
+          user: current_user
+        }, status: :ok
+      else
+        render json: {
+          error: 'Failed to update profile.',
+          details: current_user.errors.full_messages
+        }, status: :unprocessable_entity
+      end
     else
-      render json: {
-        error: 'Failed to update profile.',
-        details: current_user.errors.full_messages
-      }, status: :unprocessable_entity
+      render json: { error: 'Invalid password' }, status: :unprocessable_entity
     end
   end
 
